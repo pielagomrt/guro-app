00e352d91b57e5351563eb1451dc4cd183d97a9d
REGINA PIELAGO <pielago.mrt@gmail.com>
2021-03-25 16:21:12 +0300

    updated columns of table teachers

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,11 +2,8 @@ class Teacher < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable 
-
-  validates :firstname, presence: true   
-  validates :lastname, presence: true   
-  validates :email, presence: true
-  validates :password, presence: true
-
+         :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :subject, presence: true
 end
--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -1,73 +1,43 @@
-# require 'rails_helper'
-
-# RSpec.describe Teacher, type: :model do
-#     let(:teacher) { Teacher.new }
-#     it "1. should not save without firstname" do 
-         
-        
-#         teacher.firstname = nil
-#         teacher.lastname = "Pielago" 
-#         teacher.email = "pielago@gmail.com"
-#         teacher.password = "12345"
-#         teacher.sections = 4
-#         teacher.grading_system = 90 
-
-#         expect(teacher).to_not be_valid
-#         expect(teacher.errors).to be_present
-#         expect(teacher.errors.to_hash.keys).to include(:firstname)
-#     end 
-# end
-
 require 'rails_helper'
 
 RSpec.describe Teacher, type: :model do
-  let(:teacher) { Teacher.new }
+  let(:teacher) { described_class.new }
 
-  context 'Validations' do
-    it '1. should not save without firstname' do
-      teacher.firstname = nil
-      teacher.lastname = 'Doe'
+  context 'when validating' do
+    it '1. should not save without first name' do
+      teacher.first_name = nil
+      teacher.last_name = 'Doe'
       teacher.email = 'doe@example.com'
-      teacher.password = '12345'
+      teacher.subject = 'Science'
 
-      expect(teacher).to_not be_valid
-      expect(teacher.errors).to be_present
-      expect(teacher.errors.to_h.keys).to include(:firstname)
+      expect(teacher).not_to be_valid
     end
 
-    it '2. should not save without lastname' do
-        teacher.firstname = "Jane"
-        teacher.lastname = nil
-        teacher.email = 'doe@example.com'
-        teacher.password = '12345'
-  
-        expect(teacher).to_not be_valid
-        expect(teacher.errors).to be_present
-        expect(teacher.errors.to_h.keys).to include(:lastname)
-      end
+    it '2. should not save without last name' do
+      teacher.first_name = 'Jane'
+      teacher.last_name = nil
+      teacher.email = 'doe@example.com'
+      teacher.subject = 'Science'
 
-     it '3. should not save without email' do
-        teacher.firstname = "Jane"
-        teacher.lastname = "Doe"
-        teacher.email = nil
-        teacher.password = '12345'
-  
-        expect(teacher).to_not be_valid
-        expect(teacher.errors).to be_present
-        expect(teacher.errors.to_h.keys).to include(:email)
-      end
-     
-    it '4. should not save without password' do
-        teacher.firstname = "Jane"
-        teacher.lastname = "Doe"
-        teacher.email = 'doe@example.com'
-        teacher.password = nil
-  
-        expect(teacher).to_not be_valid
-        expect(teacher.errors).to be_present
-        expect(teacher.errors.to_h.keys).to include(:password)
-    end  
-      
+      expect(teacher).not_to be_valid
+    end
+
+    it '3. should not save without email' do
+      teacher.first_name = 'Jane'
+      teacher.last_name = 'Doe'
+      teacher.email = nil
+      teacher.subject = 'Science'
+
+      expect(teacher).not_to be_valid
+    end
+
+    it '4. should not save without subject' do
+      teacher.first_name = 'Jane'
+      teacher.last_name = 'Doe'
+      teacher.email = 'doe@example.com'
+      teacher.subject = nil
+
+      expect(teacher).not_to be_valid
+    end
   end
 end
-
