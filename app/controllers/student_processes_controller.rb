531b6d23c66f308ac988a2a521f0e84339dfd782
REGINA PIELAGO <pielago.mrt@gmail.com>
2021-03-31 18:46:52 +0300

    add student processes controller with specs

---
+++ app/controllers/student_processes_controller.rb
@@ -0,0 +1,66 @@
+class StudentProcessesController < ApplicationController
+  before_action :set_section
+  before_action :set_student
+
+  def update_student
+    @student.update(params_student)
+  end
+
+  def update_student_seatwork
+    @student.seatworks.find(params[:seatwork_id]).update(params_score)
+  end
+
+  def update_student_homework
+    @student.homeworks.find(params[:homework_id]).update(params_score)
+  end
+
+  def update_student_project
+    @student.projects.find(params[:project_id]).update(params_score)
+  end
+
+  def update_student_exam
+    @student.exams.find(params[:exam_id]).update(params_score)
+  end
+
+  def destroy_student
+    @student.destroy
+  end
+
+  def destroy_student_seatwork
+    @student.seatworks.find(params[:seatwork_id]).destroy
+  end
+
+  def destroy_student_homework
+    @student.homeworks.find(params[:homework_id]).destroy
+  end
+
+  def destroy_student_project
+    @student.projects.find(params[:project_id]).destroy
+  end
+
+  def destroy_student_exam
+    @student.exams.find(params[:exam_id]).destroy
+  end
+
+  def destroy_student_attendance
+    @student.absents.find(params[:absent_id]).destroy
+  end
+
+  private
+
+  def set_section
+    @section = Section.find_by(id: params[:id], teacher: current_teacher)
+  end
+
+  def set_student
+    @student = @section.students.find(params[:student_id])
+  end
+
+  def params_student
+    params.require(:student).permit(:first_name, :last_name, :email)
+  end
+
+  def params_score
+    params.require(:student_score).permit(:score)
+  end
+end
---
+++ spec/requests/student_processes_controller_spec.rb
@@ -0,0 +1,211 @@
+require 'rails_helper'
+
+RSpec.describe StudentProcessesController, type: :request do
+  let(:teacher) { create(:teacher_with_sections) }
+  let(:section) { teacher.sections.first }
+  let(:student) { create(:student) }
+
+  before do
+    teacher.confirm
+    sign_in(teacher)
+
+    student.section = section
+    student.seatworks << create(:student_seatwork)
+    student.homeworks << create(:student_homework)
+    student.projects << create(:student_project)
+    student.exams << create(:student_exam)
+    student.absents << create(:student_absent)
+    student.save
+  end
+
+  describe 'PUT /sections/:id/students/:student_id/update student_processes#update_student' do
+    let(:updates) { { first_name: 'Lorem', last_name: 'Ipsum', email: 'student@example.com' } }
+
+    before do
+      put student_update_path(id: section.id, student_id: student.id), params: { student: updates }
+      student.reload
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'updates the student first name' do
+      expect(student.first_name).to eq(updates[:first_name])
+    end
+
+    it 'updates the student last name' do
+      expect(student.last_name).to eq(updates[:last_name])
+    end
+
+    it 'updates the student email' do
+      expect(student.email).to eq(updates[:email])
+    end
+  end
+
+  describe 'PUT /sections/:id/students/:student_id/seatworks/:seatwork_id/update student_processes#update_student_seatwork' do
+    let(:seatwork) { student.seatworks.first }
+    let(:updates) { { score: 70 } }
+
+    before do
+      put student_update_seatwork_path(id: section.id, student_id: student.id, seatwork_id: seatwork.id), params: { student_score: updates }
+      seatwork.reload
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'updates the student seatwork score' do
+      expect(seatwork.score).to eq(updates[:score])
+    end
+  end
+
+  describe 'PUT /sections/:id/students/:student_id/homeworks/:homework_id/update student_processes#update_student_homework' do
+    let(:homework) { student.homeworks.first }
+    let(:updates) { { score: 77 } }
+
+    before do
+      put student_update_homework_path(id: section.id, student_id: student.id, homework_id: homework.id), params: { student_score: updates }
+      homework.reload
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'updates the student seatwork score' do
+      expect(homework.score).to eq(updates[:score])
+    end
+  end
+
+  describe 'PUT /sections/:id/students/:student_id/projects/:project_id/update student_processes#update_student_project' do
+    let(:project) { student.projects.first }
+    let(:updates) { { score: 75 } }
+
+    before do
+      put student_update_project_path(id: section.id, student_id: student.id, project_id: project.id), params: { student_score: updates }
+      project.reload
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'updates the student seatwork score' do
+      expect(project.score).to eq(updates[:score])
+    end
+  end
+
+  describe 'PUT /sections/:id/students/:student_id/exams/:exam_id/update student_processes#update_student_exam' do
+    let(:exam) { student.exams.first }
+    let(:updates) { { score: 90 } }
+
+    before do
+      put student_update_exam_path(id: section.id, student_id: student.id, exam_id: exam.id), params: { student_score: updates }
+      exam.reload
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'updates the student seatwork score' do
+      expect(exam.score).to eq(updates[:score])
+    end
+  end
+
+  describe 'DELETE /sections/:id/students/:student_id/delete student_processes#destroy_student' do
+    before do
+      delete student_delete_path(id: section.id, student_id: student.id)
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'deletes the section' do
+      expect { Student.find(student.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  describe 'DELETE /sections/:id/students/:student_id/seatworks/:seatwork_id/delete student_processes#destroy_student_seatwork' do
+    let(:seatwork) { student.seatworks.first }
+
+    before do
+      delete student_delete_seatwork_path(id: section.id, student_id: student.id, seatwork_id: seatwork.id)
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'deletes the section' do
+      expect { student.seatworks.find(seatwork.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  describe 'DELETE /sections/:id/students/:student_id/homeworks/:homework_id/delete student_processes#destroy_student_homework' do
+    let(:homework) { student.homeworks.first }
+
+    before do
+      delete student_delete_homework_path(id: section.id, student_id: student.id, homework_id: homework.id)
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'deletes the section' do
+      expect { student.homeworks.find(homework.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  describe 'DELETE /sections/:id/students/:student_id/projects/:project_id/delete student_processes#destroy_student_project' do
+    let(:project) { student.projects.first }
+
+    before do
+      delete student_delete_project_path(id: section.id, student_id: student.id, project_id: project.id)
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'deletes the section' do
+      expect { student.projects.find(project.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  describe 'DELETE /sections/:id/students/:student_id/exams/:exam_id/delete student_processes#destroy_student_exam' do
+    let(:exam) { student.exams.first }
+
+    before do
+      delete student_delete_exam_path(id: section.id, student_id: student.id, exam_id: exam.id)
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'deletes the section' do
+      expect { student.exams.find(exam.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  describe 'DELETE /sections/:id/students/:student_id/absences/:absent_id/delete student_processes#destroy_student_attendance' do
+    let(:absent) { student.absents.first }
+
+    before do
+      delete student_delete_attendance_path(id: section.id, student_id: student.id, absent_id: absent.id)
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'deletes the section' do
+      expect { student.absents.find(absent.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
