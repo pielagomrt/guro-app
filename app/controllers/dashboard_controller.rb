0d45b83d24907200c72f1ddd275ac254e9dad147
REGINA PIELAGO <pielago.mrt@gmail.com>
2021-04-17 21:18:43 +0300

    close github issue

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,5 +7,32 @@ class DashboardController < ApplicationController
 
   def dashboard
     @youtube_videos = Youtube.search(current_teacher.subject)
+
+    @students = []
+    section.each { |section| @students << section.students.group(:section_id).count.values }
+    @student_count = @students.map(&:sum)
+
+    @seatworks = []
+    @homeworks = []
+    @projects = []
+    @exams = []
+    section_quarters.each do |category|
+      @seatworks << Quarter::Seatwork.where(quarter_id: category[:id]).count
+      @homeworks << Quarter::Homework.where(quarter_id: category[:id]).count
+      @projects << Quarter::Project.where(quarter_id: category[:id]).count
+      @exams << Quarter::Exam.where(quarter_id: category[:id]).count
+    end
+  end
+
+  private
+
+  def section
+    @sections = current_teacher.sections
+  end
+
+  def section_quarters
+    @quarters = []
+    section.each { |section| @quarters << section.quarters }
+    @section_quarters = @quarters.flatten
   end
 end
